16a36df978dbc2153e923b5d1f94f3e267657c9c
Harshit <richhariyaharshit684@gmail.com>
2024-12-03 19:51:44 +0530

    update

--- newsapp/components/NewsList.tsx
+++ newsapp/components/NewsList.tsx
@@ -25,7 +25,7 @@ const NewsList = ({ post }: { post: startuptypecard} ) => {
       </div>
 
 
-      <div className='flex-between mt-5 gap-5' >
+      <div className='flex-between mt-5 gap-4' >
         <div className='flex-1'>
           <Link href={`/user/${author?._id}`}>
           <p className='text-16-medium line-clamp-1'>{author?.name}</p>
